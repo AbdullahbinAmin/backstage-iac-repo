634a2e060b2d792e891793447fb524a59e86e0c5
AbdullahbinAmin <abdullahbinaminmeo@gmail.com>
2025-07-25 01:33:51 -0400

    Add initial S3 bucket Terraform configuration

---
+++ main.tf
@@ -0,0 +1,20 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1" # You can change this to your preferred region, e.g., "eu-west-1"
+}
+
+# Define a simple S3 bucket
+resource "aws_s3_bucket" "my_demo_bucket" {
+  bucket = "s3-bucket-backstage-demo" # IMPORTANT: REPLACE THIS WITH A GLOBALLY UNIQUE NAME
+  tags = {
+    Environment = "Demo"
+    Project     = "Backstage"
+    Owner       = "Abdullah"
+    Visibility  = "Public" # We'll use this tag for policy enforcement later
+  }
+}
+
+# Output the bucket name for verification
+output "bucket_name" {
+  value = aws_s3_bucket.my_demo_bucket.bucket
+}
